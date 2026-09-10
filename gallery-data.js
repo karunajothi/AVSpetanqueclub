a01ad0ca075f5381cd05edb6ecbec9c5ae48249f
A. Karunajothi <karunajothi.ayyanar@gmail.com>
2026-09-10 18:27:59 +0200

    Fix gallery rotation and externalize gallery data

---
+++ gallery-data.js
@@ -0,0 +1,110 @@
+const eventsData = [
+  {
+    id: 1,
+    title: "Avs 1st yearState level Tournament",
+    desc: "Organised by AVS Gorimedu Brothers Pétanque Club (13/01/2025 & 14/01/2025)",
+    img1: "public/event1.jpeg",
+    img2: "public/event1-2.jpeg",
+    lbl1: "Event Photo",
+    lbl2: "Event Highlight"
+  },
+  {
+    id: 2,
+    title: "Brothers Petanque Club Tournament",
+    desc: "Organised by Brothers Petanque Club, Kombakkam (21/02/2026 & 22/02/2026)",
+    img1: "public/event2.jpeg",
+    img2: "public/event2.jpeg",
+    lbl1: "Match Action",
+    lbl2: "Participants"
+  },
+  {
+    id: 3,
+    title: "Thanthai Periyar Sports Club Tournament",
+    desc: "Participated with 09 teams (28/02/2026 & 01/03/2026)",
+    img1: "public/event3.jpeg",
+    img2: "public/event3.jpeg",
+    lbl1: "Knockout Round",
+    lbl2: "Players"
+  },
+  {
+    id: 4,
+    title: "Friendly Match vs NR AKD Petanque Club",
+    desc: "Friendly match played on 08/03/2026",
+    img1: "public/event4.jpeg",
+    img2: "public/event4.jpeg",
+    lbl1: "Ground Photos",
+    lbl2: "Teams"
+  },
+  {
+    id: 5,
+    title: "Inter Club Knock Out Tournament",
+    desc: "Conducted internal club tournament on 05/04/2026",
+    img1: "public/event5.jpeg",
+    img2: "public/event5.jpeg",
+    lbl1: "Semi-Finals",
+    lbl2: "Finalists"
+  },
+  {
+    id: 6,
+    title: "Lucky Doubles Tournament",
+    desc: "Winners: JK & Aravind (02/05/2026 & 03/05/2026)",
+    img1: "public/event6.jpeg",
+    img2: "public/event6.jpeg",
+    lbl1: "Winners Ceremony",
+    lbl2: "Runners"
+  },
+  {
+    id: 7,
+    title: "AVS Premier League",
+    desc: "Champions: Mohan & Venkat (09/05/2026 to 30/05/2026)",
+    img1: "public/event7.jpeg",
+    img2: "public/event7.jpeg",
+    lbl1: "League Matches",
+    lbl2: "Champions"
+  },
+  {
+    id: 8,
+    title: "AVS Interclub Tournament (SRI TROPHY)",
+    desc: "Sponsored by Alex (20/06/2026 & 21/06/2026)",
+    img1: "public/event8.jpeg",
+    img2: "public/event8.jpeg",
+    lbl1: "Trophy Presentation",
+    lbl2: "Group Photo"
+  },
+  {
+    id: 9,
+    title: "AVS Inter Club Tournament",
+    desc: "Winners: Rajesh & Mohan (12/07/2026)",
+    img1: "public/event9.jpeg",
+    img2: "public/event9.jpeg",
+    lbl1: "Winners",
+    lbl2: "Runners Up"
+  },
+  {
+    id: 10,
+    title: "NPAP State Championship",
+    desc: "Participated on 01/08/2026 & 02/08/2026",
+    img1: "public/event10.jpeg",
+    img2: "public/event10.jpeg",
+    lbl1: "State Event",
+    lbl2: "AVS Squad"
+  },
+  {
+    id: 11,
+    title: "Friendship Trophy (Triples)",
+    desc: "Winners: JK, Ram & Ilango (08/08/2026 & 09/08/2026)",
+    img1: "public/event11.jpeg",
+    img2: "public/event11.jpeg",
+    lbl1: "Ground Action",
+    lbl2: "Friendship Trophy"
+  },
+  {
+    id: 12,
+    title: "Independence Day Trophy",
+    desc: "Sponsored by Prabhu (15/08/2026 & 16/08/2026)",
+    img1: "public/event12.jpeg",
+    img2: "public/event12.jpeg",
+    lbl1: "Celebrations",
+    lbl2: "Trophy Winners"
+  }
+];
